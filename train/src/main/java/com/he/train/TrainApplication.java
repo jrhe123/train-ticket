13d6e8bdf36400d7ebc0ab96b594937b926d917f
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-07-17 20:41:39 -0400

    test app

--- train/src/main/java/com/he/train/TrainApplication.java
+++ train/src/main/java/com/he/train/TrainApplication.java
@@ -1,13 +1,17 @@
 package com.he.train;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TrainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TrainApplication.class, args);
+		SpringApplication application = 
+				new SpringApplication(TrainApplication.class);
+		application.setWebApplicationType(WebApplicationType.SERVLET);
+		application.run(args);
 	}
 
 }
---
+++ train/src/main/java/com/he/train/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.he.train.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+	@GetMapping("/demo")
+	public String testing() {
+		return "123";
+	}
+}
