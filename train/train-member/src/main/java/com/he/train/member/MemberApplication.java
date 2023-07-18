84c1c879cd4d437c4f63f194cd83044a0562252c
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-07-17 21:55:11 -0400

    add log file

--- train/train-member/src/main/java/com/he/train/member/MemberApplication.java
+++ train/train-member/src/main/java/com/he/train/member/MemberApplication.java
@@ -1,15 +1,23 @@
 package com.he.train.member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class MemberApplication {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(MemberApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(MemberApplication.class);
 		application.setWebApplicationType(WebApplicationType.SERVLET);
-		application.run(args);
+		
+		Environment env = application.run(args).getEnvironment();
+		LOGGER.info(">>>>> Start [MemberApplication]");
+		LOGGER.info(">>>>> Address: http://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 }
--- train/train-member/src/main/java/com/he/train/member/controller/TestController.java
+++ train/train-member/src/main/java/com/he/train/member/controller/TestController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/member")
+@RequestMapping("/test")
 public class TestController {
 
-	@GetMapping("/test")
+	@GetMapping("")
 	public String test() {
 		return "123";
 	}
