eb001b2e4129a2332ca53778e70f24a002aa754a
jrhe123 <keith_he@outlook.com>
2024-12-28 20:51:05 -0500

    Ant design & icon

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,18 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import Antd from 'ant-design-vue';
+import * as Icons from '@ant-design/icons-vue';
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import 'ant-design-vue/dist/reset.css';
+
+const app = createApp(App);
+
+app.use(Antd).use(store).use(router).mount('#app');
+
+const icons = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a-button>1123</a-button><StepForwardOutlined />
   </div>
 </template>
