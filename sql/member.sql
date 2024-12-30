050c45acd0ddac3841c993fb1dffd2b178b606b4
jrhe123 <keith_he@outlook.com>
2024-12-29 20:07:36 -0500

    BE: passenger save endpoint

--- sql/member.sql
+++ sql/member.sql
@@ -5,3 +5,17 @@ CREATE TABLE `member` (
     PRIMARY KEY (`id`),
     UNIQUE KEY `mobile_unique` (`mobile`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='会员';
+
+
+DROP TABLE IF EXISTS `passenger`;
+CREATE TABLE `passenger` (
+                             `id` BIGINT NOT NULL COMMENT 'id',
+                             `member_id` BIGINT NOT NULL COMMENT '会员id',
+                             `name` VARCHAR(20) NOT NULL COMMENT '姓名',
+                             `id_card` VARCHAR(18) NOT NULL COMMENT '身份证',
+                             `type` CHAR(1) NOT NULL COMMENT '旅客类型[PassengerTypeEnum]',
+                             `create_time` DATETIME(3) COMMENT '新增时间',
+                             `update_time` DATETIME(3) COMMENT '修改时间',
+                             PRIMARY KEY (`id`),
+                             INDEX `member_id_index` (`member_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='乘车人';
---
+++ train/member/src/main/java/com/he/train/member/service/PassengerService.java
@@ -0,0 +1,27 @@
+package com.he.train.member.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import com.he.train.common.util.SnowUtil;
+import com.he.train.member.domain.Passenger;
+import com.he.train.member.mapper.PassengerMapper;
+import com.he.train.member.req.PassengerSaveReq;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PassengerService {
+
+    @Resource
+    private PassengerMapper passengerMapper;
+
+    public void save(PassengerSaveReq req) {
+        DateTime now = DateTime.now();
+        Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+
+        passenger.setId(SnowUtil.getSnowflakeNextId());
+        passenger.setCreateTime(now);
+        passenger.setUpdateTime(now);
+        passengerMapper.insert(passenger);
+    }
+}
